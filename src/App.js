514e47f53548c9fe6300439fcb7b650bd071101b
Johan Pedro Inês <johan.ines@matera.com>
2019-03-21 12:28:50 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import style from './App.scss';
+import { SortableContainer, SortableElement} from 'react-sortable-hoc';
+import arrayMove from 'array-move';
 
-class App extends Component {
-  render() {
+console.log(style);
+const SortableItem = SortableElement(({value}) => {
+    return <li className="item" style={{listStyle: 'none'}}><img src={`./image/mm_${value}.png`} alt={value}/></li>
+});
+
+const SortableList = SortableContainer(({items}) => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <ul onClick={(e) => console.log(e.target)}
+            className="list horizontalList">
+            {items.map((value, index) => (
+                <SortableItem key={`item-${index}`} index={index} value={value}/>
+            ))}
+        </ul>
     );
-  }
+});
+class App extends Component {
+    state = {
+        items: [
+            "meta",
+            "curiosidade",
+            "liberdade",
+            "status",
+            "aceitacao",
+            "relacao",
+            "honra",
+            "maestria",
+            "poder",
+            "ordem",
+        ],
+        openImg: false
+    };
+    onSortEnd = ({oldIndex, newIndex,...other}) => {
+        if(oldIndex === newIndex){
+            return this.setState({openImg: this.state.items[oldIndex]});
+        }
+        this.setState(({items}) => ({
+            items: arrayMove(items, oldIndex, newIndex),
+        }));
+    };
+
+    closeImg = () => {
+        this.setState({openImg: false});
+    };
+
+    render() {
+        return <>
+            <SortableList
+                axis="xy"
+                items={this.state.items} onSortEnd={this.onSortEnd} />
+            {
+                this.state.openImg && <div className="background" onClick={this.closeImg}><img className="img-open" src={`./image/mm_${this.state.openImg}.png`} alt={this.state.openImg}/></div>
+            }
+        </>;
+    }
 }
 
 export default App;
