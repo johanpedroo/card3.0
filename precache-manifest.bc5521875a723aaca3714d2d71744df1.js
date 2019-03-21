9a2b3b10376ce8a041a68d5082a2d05af61b219f
Johan Pedro Inês <johan.ines@matera.com>
2019-03-21 19:14:17 +0000

    gh-pages update

---
+++ precache-manifest.bc5521875a723aaca3714d2d71744df1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "29d102239958efa9400a",
+    "url": "/card3.0/static/js/runtime~main.1e0cd191.js"
+  },
+  {
+    "revision": "8649dbe106c9dad24fa1",
+    "url": "/card3.0/static/js/main.287b6dda.chunk.js"
+  },
+  {
+    "revision": "c7dd353b22811ddfb6e5",
+    "url": "/card3.0/static/js/2.436c7bd3.chunk.js"
+  },
+  {
+    "revision": "8649dbe106c9dad24fa1",
+    "url": "/card3.0/static/css/main.77369f52.chunk.css"
+  },
+  {
+    "revision": "2d75a2d09734956432c037612958fb18",
+    "url": "/card3.0/index.html"
+  }
+];
